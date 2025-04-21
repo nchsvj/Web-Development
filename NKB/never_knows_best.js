aa1fda3ee5a8c09019f1a46d35da6db14d468d3b
Damir <159621868+nchsvj@users.noreply.github.com>
2025-04-21 09:07:29 +0500

    Add files via upload

---
+++ NKB/never_knows_best.js
@@ -0,0 +1,51 @@
+// alert("Добро пожаловать на сайт!");
+
+// let name;
+// name = prompt('What is your name?');
+// alert(`Hi, ${name}!`);
+//
+// let age;
+// age = prompt('What is your age?');
+//
+// if (age >= 18) {
+//     alert(`You are teenager !!!`);
+// }   else {
+//     alert(`You are kid :)`);
+// }
+
+
+// let isBoss = confirm('Are you boss?');
+// alert(isBoss);
+
+
+// let question = prompt('Какое "официальное" название JavaScript ?', '');
+// if (question === 'ECMAScript'.toLowerCase()) {
+//     alert('Верно!');
+// }   else {
+//     alert('Не знаете? "ECMAScript"!');
+// }
+
+
+// let number = prompt('Number: ');
+// if (number > 0) {
+//     alert('1');
+// }   else if (number < 0) {
+//     alert('-1');
+// }   else {
+//     alert('0');
+// }
+
+
+// let result;
+//
+// result = (a + b < 4) ? 'Мало' : 'Много';
+
+
+// let login = prompt('Enter your login credentials');
+// let message = (login === 'Сотрудник'.toLowerCase()) ? 'Привет' :
+//     (login === 'Директор'.toLowerCase()) ? 'Здравствуйте' :
+//     (login === '') ? 'Нет логина' :
+//     '';
+// alert(message);
+
+
